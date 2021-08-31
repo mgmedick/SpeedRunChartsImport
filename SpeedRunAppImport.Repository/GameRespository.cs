f8b7bf41fd5ef2a73e0281702f9ae4cb7c59ebc8
Mike Medick <mgmedick@live.com>
2021-08-30 20:20:56 -0500

    More changes.

--- SpeedRunAppImport.Repository/GameRespository.cs
+++ SpeedRunAppImport.Repository/GameRespository.cs
@@ -698,12 +698,14 @@ namespace SpeedRunAppImport.Repository
                 db.OneTimeCommandTimeout = 32767;
                 db.Execute(@"DECLARE @@Sql NVARCHAR(MAX) = ''
 
-                            SELECT @@Sql = 'ALTER TABLE [' + s.name + '].[' + o.name + '] NOCHECK CONSTRAINT all'
-                            FROM sys.foreign_keys i
-                            INNER JOIN sys.objects o ON i.parent_object_id = o.OBJECT_ID
-                            INNER JOIN sys.schemas s ON o.schema_id = s.schema_id
+                            SELECT @@Sql = STUFF((SELECT DISTINCT ' ALTER TABLE [' + s.name + '].[' + o.name + '] NOCHECK CONSTRAINT all'
+			                            FROM sys.foreign_keys i
+			                            INNER JOIN sys.objects o ON i.parent_object_id = o.OBJECT_ID
+			                            INNER JOIN sys.schemas s ON o.schema_id = s.schema_id
+			                              FOR XML PATH ('')
+			                            ), 1, 1, '')
 
-                            EXEC (@@Sql)");
+                            SELECT @@Sql");
 
                 foreach (var game in gamesList)
                 {
@@ -943,12 +945,14 @@ namespace SpeedRunAppImport.Repository
 
                 db.Execute(@"DECLARE @@Sql NVARCHAR(MAX) = ''
 
-                            SELECT @@Sql = 'ALTER TABLE [' + s.name + '].[' + o.name + '] WITH CHECK CHECK CONSTRAINT [' + i.name + ']'
-                            FROM sys.foreign_keys i
-                            INNER JOIN sys.objects o ON i.parent_object_id = o.OBJECT_ID
-                            INNER JOIN sys.schemas s ON o.schema_id = s.schema_id
+                            SELECT @@Sql = STUFF((SELECT DISTINCT ' ALTER TABLE [' + s.name + '].[' + o.name + '] WITH CHECK CHECK CONSTRAINT [' + i.name + ']'
+			                            FROM sys.foreign_keys i
+			                            INNER JOIN sys.objects o ON i.parent_object_id = o.OBJECT_ID
+			                            INNER JOIN sys.schemas s ON o.schema_id = s.schema_id
+			                              FOR XML PATH ('')
+			                            ), 1, 1, '')
 
-                            EXEC (@@Sql)");
+                            SELECT @@Sql");
             }
         }
 

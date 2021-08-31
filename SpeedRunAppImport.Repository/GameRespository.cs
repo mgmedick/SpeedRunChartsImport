53383383a9cad39efce79fdc8f2e06684c5830bf
Mike Medick <mgmedick@live.com>
2021-08-30 21:33:25 -0500

    removing disable keys.

--- SpeedRunAppImport.Repository/GameRespository.cs
+++ SpeedRunAppImport.Repository/GameRespository.cs
@@ -695,17 +695,17 @@ namespace SpeedRunAppImport.Repository
 
             using (IDatabase db = DBFactory.GetDatabase())
             {
-                db.OneTimeCommandTimeout = 32767;
-                db.Execute(@"DECLARE @@Sql NVARCHAR(MAX) = ''
+                //db.OneTimeCommandTimeout = 32767;
+                //db.Execute(@"DECLARE @@Sql NVARCHAR(MAX) = ''
 
-                            SELECT @@Sql = STUFF((SELECT DISTINCT ' ALTER TABLE [' + s.name + '].[' + o.name + '] NOCHECK CONSTRAINT all'
-			                            FROM sys.foreign_keys i
-			                            INNER JOIN sys.objects o ON i.parent_object_id = o.OBJECT_ID
-			                            INNER JOIN sys.schemas s ON o.schema_id = s.schema_id
-			                              FOR XML PATH ('')
-			                            ), 1, 1, '')
+                //            SELECT @@Sql = STUFF((SELECT DISTINCT ' ALTER TABLE [' + s.name + '].[' + o.name + '] NOCHECK CONSTRAINT all'
+                //               FROM sys.foreign_keys i
+                //               INNER JOIN sys.objects o ON i.parent_object_id = o.OBJECT_ID
+                //               INNER JOIN sys.schemas s ON o.schema_id = s.schema_id
+                //                 FOR XML PATH ('')
+                //               ), 1, 1, '')
 
-                            EXEC(@@Sql)");
+                //            EXEC(@@Sql)");
 
                 foreach (var game in gamesList)
                 {
@@ -943,17 +943,17 @@ namespace SpeedRunAppImport.Repository
                     count++;
                 }
 
-                db.OneTimeCommandTimeout = 32767;
-                db.Execute(@"DECLARE @@Sql NVARCHAR(MAX) = ''
+                //db.OneTimeCommandTimeout = 32767;
+                //db.Execute(@"DECLARE @@Sql NVARCHAR(MAX) = ''
 
-                            SELECT @@Sql = STUFF((SELECT DISTINCT ' ALTER TABLE [' + s.name + '].[' + o.name + '] WITH CHECK CHECK CONSTRAINT [' + i.name + ']'
-			                            FROM sys.foreign_keys i
-			                            INNER JOIN sys.objects o ON i.parent_object_id = o.OBJECT_ID
-			                            INNER JOIN sys.schemas s ON o.schema_id = s.schema_id
-			                              FOR XML PATH ('')
-			                            ), 1, 1, '')
+                //            SELECT @@Sql = STUFF((SELECT DISTINCT ' ALTER TABLE [' + s.name + '].[' + o.name + '] WITH CHECK CHECK CONSTRAINT [' + i.name + ']'
+                //               FROM sys.foreign_keys i
+                //               INNER JOIN sys.objects o ON i.parent_object_id = o.OBJECT_ID
+                //               INNER JOIN sys.schemas s ON o.schema_id = s.schema_id
+                //                 FOR XML PATH ('')
+                //               ), 1, 1, '')
 
-                            EXEC(@@Sql)");
+                //            EXEC(@@Sql)");
             }
         }
 
